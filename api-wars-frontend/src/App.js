1b1e098165900a455bc0dbc4bfb742e4a74007c3
Adél Hidvégi <adel.hidvegi@gmail.com>
2022-02-22 16:57:20 +0100

    add a not working redirection

--- api-wars-frontend/src/App.js
+++ api-wars-frontend/src/App.js
@@ -2,36 +2,38 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-    const handleSignUpOnSuccess = () => {
-        console.log("done");
-    };
-    const handleSignInOnSuccess = () => {
-        console.log("done it");
-    };
+  //let navigate = useNavigate();
+  const handleSignUpOnSuccess = () => {
+    //return navigate("/login");
+  };
+  const handleSignInOnSuccess = () => {
+    console.log("done it");
+  };
 
-    return (
-        <div className="App">
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/register">Registration</Link>
-                <Link to="login">Login</Link>
-                Show username if logined
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route
-                    path="/register"
-                    element={<Register onSuccess={handleSignUpOnSuccess} />}
-                />
-                <Route
-                    path="/login"
-                    element={<Login onSuccess={handleSignInOnSuccess} />}
-                />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/register">Registration</Link>
+        <Link to="login">Login</Link>
+        Show username if logined
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/register"
+          element={<Register onSuccess={handleSignUpOnSuccess} />}
+        />
+        <Route
+          path="/login"
+          element={<Login onSuccess={handleSignInOnSuccess} />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- api-wars-frontend/src/Register.jsx
+++ api-wars-frontend/src/Register.jsx
@@ -7,116 +7,123 @@ import FilledInput from "@mui/material/FilledInput";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Button from "@mui/material/Button";
+import FormHelperText from "@mui/material/FormHelperText";
+import { useNavigate } from "react-router-dom";
 
 const signup = (userName, password) => {
-    let status = true;
+  let status = true;
 
-    return fetch("/register", {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify({ userName, password }),
+  return fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ userName, password }),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        status = false;
+      }
+      return res;
     })
-        .then((res) => {
-            if (!res.ok) {
-                status = false;
-            }
-            return res;
-        })
-        .then((res) => {
-            return res.json();
-        })
-        .then((info) => {
-            if (status) {
-                return info;
-            }
-            throw info;
-        });
+    .then((res) => {
+      return res.json();
+    })
+    .then((info) => {
+      if (status) {
+        return info;
+      }
+      throw info;
+    });
 };
 
 const Register = (props) => {
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [helperText, setHelperText] = useState("");
 
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  let navigate = useNavigate();
 
-    const handleSignUp = (userName, password) => {
-        setError(null);
-        setLoading(true);
-        signup(userName, password)
-            .then(() => {
-                props.onSuccess();
-            })
-            .catch((err) => {
-                setError(err);
-                console.log(err);
-            })
-            .finally(() => {
-                setLoading(false);
-                setUserName("");
-                setPassword("");
-            });
-    };
+  const handleSignUp = (userName, password) => {
+    setError(null);
+    setLoading(true);
+    signup(userName, password)
+      .then(() => {
+        props.onSuccess();
+      })
+      .catch((err) => {
+        setError(err);
+        setHelperText(err.message);
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+        setUserName("");
+        setPassword("");
+      });
+    return navigate("/login");
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (userName || password) {
-            return handleSignUp(userName, password);
-        }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userName || password) {
+      return handleSignUp(userName, password);
+    }
+  };
 
-    const handleUserNameChange = (e) => {
-        setUserName(e.target.value);
-    };
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    return (
-        <Box
-            component="form"
-            sx={{
-                "& > :not(style)": { m: 1 },
-            }}
-            noValidate
-            autoComplete="off"
-            onSubmit={handleSubmit}
-        >
-            <FormControl variant="filled">
-                <InputLabel htmlFor="component-filled">Username</InputLabel>
-                <FilledInput
-                    id="component-filled"
-                    type="text"
-                    className="input"
-                    value={userName}
-                    onChange={handleUserNameChange}
-                    disabled={loading}
-                />
-            </FormControl>
-            <FormControl variant="filled">
-                <InputLabel htmlFor="component-filled2">Password</InputLabel>
-                <FilledInput
-                    id="component-filled2"
-                    type="password"
-                    className="input"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    disabled={loading}
-                />
-            </FormControl>
-            <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                className="submit-btn"
-                disabled={loading}
-            >
-                Register
-            </Button>
-        </Box>
-    );
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1 },
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
+      <FormControl variant="filled">
+        <InputLabel htmlFor="component-filled">Username</InputLabel>
+        <FilledInput
+          id="component-filled"
+          type="text"
+          className="input"
+          value={userName}
+          onChange={handleUserNameChange}
+          disabled={loading}
+        />
+      </FormControl>
+      <FormControl variant="filled">
+        <InputLabel htmlFor="component-filled2">Password</InputLabel>
+        <FilledInput
+          id="component-filled2"
+          type="password"
+          className="input"
+          value={password}
+          onChange={handlePasswordChange}
+          disabled={loading}
+        />
+      </FormControl>
+      <FormHelperText>{helperText}</FormHelperText>
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+        className="submit-btn"
+        disabled={loading}
+      >
+        Register
+      </Button>
+    </Box>
+  );
 };
 
 export default Register;
