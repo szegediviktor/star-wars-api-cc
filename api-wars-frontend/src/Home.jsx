e00a2ab0480d547846c46b3f3b55fc1353bcce47
BuzinszkiCintia <buzinszki.cintia@gmail.com>
2022-02-23 14:34:52 +0100

    update table

--- api-wars-frontend/src/Home.jsx
+++ api-wars-frontend/src/Home.jsx
@@ -11,51 +11,77 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
 const Home = () => {
-    const [planets, setPlanets] = useState([]);
+  const [planets, setPlanets] = useState([]);
 
-    useEffect(() => {
-        fetch("https://swapi.dev/api/planets")
-            .then((res) => res.json())
-            .then((data) => {
-                setPlanets(data);
-            });
-    }, []);
+  useEffect(() => {
+    fetch("https://swapi.dev/api/planets")
+      .then((res) => res.json())
+      .then((data) => {
+        setPlanets(data);
+      });
+  }, []);
 
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Diameter</TableCell>
-                        <TableCell>Climate</TableCell>
-                        <TableCell>Terrain</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {planets.results?.map((planet) => (
-                        <TableRow
-                            key={planet.name}
-                            sx={{
-                                "&:last-child td, &:last-child th": {
-                                    border: 0,
-                                },
-                            }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {planet.name}
-                            </TableCell>
-                            <TableCell>{planet.diameter}</TableCell>
-                            <TableCell>{planet.climate}</TableCell>
-                            <TableCell>{planet.terrain}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  //formátum átalakítás
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Diameter</TableCell>
+            <TableCell>Climate</TableCell>
+            <TableCell>Terrain</TableCell>
+            <TableCell>Surface Water Percentage</TableCell>
+            <TableCell>Population</TableCell>
+            <TableCell>Residents</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {planets.results?.map((planet) => (
+            <TableRow
+              key={planet.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {planet.name}
+              </TableCell>
+              <TableCell>{`${numberWithCommas(planet.diameter)} km`}</TableCell>
+              <TableCell>{planet.climate}</TableCell>
+              <TableCell>{planet.terrain}</TableCell>
+              <TableCell>
+                {planet.surface_water === "unknown"
+                  ? planet.surface_water
+                  : `${planet.surface_water} %`}
+              </TableCell>
+              <TableCell>
+                {planet.population === "unknown"
+                  ? planet.population
+                  : `${numberWithCommas(planet.population)} people`}
+              </TableCell>
+              <TableCell>
+                {planet.residents.length > 0 ? (
+                  <Button>
+                    {planet.residents.length === 1
+                      ? `${planet.residents.length} resident`
+                      : `${planet.residents.length} residents`}
+                  </Button>
+                ) : (
+                  "No known residents"
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default Home;
