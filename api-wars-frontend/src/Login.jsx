c6ced2f452cb6670cbe7a419d3a863659463dc35
szegediviktor <szeg.viktor@gmail.com>
2022-02-24 17:01:13 +0100

    add mui-style to login and register

--- api-wars-frontend/src/Login.jsx
+++ api-wars-frontend/src/Login.jsx
@@ -8,108 +8,143 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Button from "@mui/material/Button";
 import FormHelperText from "@mui/material/FormHelperText";
+import { useNavigate } from "react-router-dom";
+import { Grid } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const Login = (props) => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [helperText, setHelperText] = useState("");
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [helperText, setHelperText] = useState("");
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-  const signin = (userName, password) => {
-    return fetch("/login", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ userName, password }),
-    }).then((res) => {
-      if (res.ok) {
-        return res.json().then((data) => props.addUser(data));
-      }
-      throw new Error("Wrong credentials");
-    });
-  };
+    const signin = (userName, password) => {
+        return fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({ userName, password }),
+        }).then((res) => {
+            if (res.ok) {
+                return res.json().then((data) => props.addUser(data));
+            }
+            throw new Error("Wrong credentials");
+        });
+    };
 
-  const handleSignIn = (userName, password) => {
-    setError(null);
-    setLoading(true);
-    signin(userName, password)
-      .then(() => {
-        props.onSuccess();
-      })
-      .catch((err) => {
-        setError(err);
-        console.log(err);
-      })
-      .finally(() => {
-        setLoading(false);
-        setUserName("");
-        setPassword("");
-      });
-  };
+    const handleSignIn = (userName, password) => {
+        setError(null);
+        setLoading(true);
+        signin(userName, password)
+            .then(() => {
+                props.onSuccess();
+            })
+            .catch((err) => {
+                setError(err);
+                console.log(err);
+            })
+            .finally(() => {
+                setLoading(false);
+                setUserName("");
+                setPassword("");
+            });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (userName || password) {
-      return handleSignIn(userName, password);
-    }
-    if (!userName || !password) {
-      setHelperText("Please, fill in both fields.");
-    }
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (userName || password) {
+            return handleSignIn(userName, password);
+        }
+        if (!userName || !password) {
+            setHelperText("Please, fill in both fields.");
+        }
+    };
 
-  const handleUserNameChange = (e) => {
-    setUserName(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+    const handleUserNameChange = (e) => {
+        setUserName(e.target.value);
+    };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
-  return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1 },
-      }}
-      noValidate
-      autoComplete="off"
-      onSubmit={handleSubmit}
-    >
-      <FormControl variant="filled">
-        <InputLabel htmlFor="component-filled">Username</InputLabel>
-        <FilledInput
-          id="component-filled"
-          type="text"
-          className="input"
-          value={userName}
-          onChange={handleUserNameChange}
-          disabled={loading}
-        />
-      </FormControl>
-      <FormControl variant="filled">
-        <InputLabel htmlFor="component-filled2">Password</InputLabel>
-        <FilledInput
-          id="component-filled2"
-          type="password"
-          className="input"
-          value={password}
-          onChange={handlePasswordChange}
-          disabled={loading}
-        />
-      </FormControl>
-      <FormHelperText>{helperText}</FormHelperText>
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-        className="submit-btn"
-        disabled={loading}
-      >
-        Login
-      </Button>
-    </Box>
-  );
+    return (
+        <Box
+            component="form"
+            sx={{
+                "& > :not(style)": { m: 1 },
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+            width="100%"
+            height="100%"
+        >
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Grid item xs={12} md={6} lg={3} m={1} pt={1}>
+                    <Typography variant="h3" gutterBottom component="div">
+                        Login
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={6} lg={3} m={1} pt={5}>
+                    <FormControl variant="filled">
+                        <InputLabel
+                            htmlFor="component-filled"
+                            color="secondary"
+                        >
+                            Username
+                        </InputLabel>
+                        <FilledInput
+                            id="component-filled"
+                            type="text"
+                            className="input"
+                            value={userName}
+                            onChange={handleUserNameChange}
+                            disabled={loading}
+                            color="secondary"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} md={6} lg={3} m={3} pt={2}>
+                    <FormControl variant="filled">
+                        <InputLabel
+                            htmlFor="component-filled2"
+                            color="secondary"
+                        >
+                            Password
+                        </InputLabel>
+                        <FilledInput
+                            id="component-filled2"
+                            type="password"
+                            className="input"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            disabled={loading}
+                            color="secondary"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} md={6} lg={3} m={3} pt={2}>
+                    <FormHelperText>{helperText}</FormHelperText>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        className="submit-btn"
+                        disabled={loading}
+                    >
+                        Login
+                    </Button>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 };
 
 export default Login;
--- api-wars-frontend/src/Register.jsx
+++ api-wars-frontend/src/Register.jsx
@@ -9,156 +9,164 @@ import InputLabel from "@mui/material/InputLabel";
 import Button from "@mui/material/Button";
 import FormHelperText from "@mui/material/FormHelperText";
 import { useNavigate } from "react-router-dom";
+import Typography from "@mui/material/Typography";
 
 // grid
 import { Grid } from "@mui/material";
 
 const signup = (userName, password) => {
-  let status = true;
+    let status = true;
 
-  return fetch("/register", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify({ userName, password }),
-  })
-    .then((res) => {
-      if (!res.ok) {
-        status = false;
-      }
-      return res;
+    return fetch("/register", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({ userName, password }),
     })
-    .then((res) => {
-      return res.json();
-    })
-    .then((info) => {
-      if (status) {
-        return info;
-      }
-      throw info;
-    });
+        .then((res) => {
+            if (!res.ok) {
+                status = false;
+            }
+            return res;
+        })
+        .then((res) => {
+            return res.json();
+        })
+        .then((info) => {
+            if (status) {
+                return info;
+            }
+            throw info;
+        });
 };
 
 const Register = (props) => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [helperText, setHelperText] = useState("");
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [helperText, setHelperText] = useState("");
 
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  // const [isSuccesful, setIsSuccesful] = useState(false);
-  let navigate = useNavigate();
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    // const [isSuccesful, setIsSuccesful] = useState(false);
+    let navigate = useNavigate();
 
-  const handleSignUp = (userName, password) => {
-    setError(null);
-    setLoading(true);
-    signup(userName, password)
-      .then(() => {
-        props.onSuccess();
-      })
-      .then(() => navigate("/login"))
-      .catch((err) => {
-        setError(err);
-        setHelperText(err.message);
-        console.log(err);
-      })
-      .finally(() => {
-        setLoading(false);
-        setUserName("");
-        setPassword("");
-      });
-  };
+    const handleSignUp = (userName, password) => {
+        setError(null);
+        setLoading(true);
+        signup(userName, password)
+            .then(() => {
+                props.onSuccess();
+            })
+            .then(() => navigate("/login"))
+            .catch((err) => {
+                setError(err);
+                setHelperText(err.message);
+                console.log(err);
+            })
+            .finally(() => {
+                setLoading(false);
+                setUserName("");
+                setPassword("");
+            });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (userName || password) {
-      return handleSignUp(userName, password);
-    }
-    // if (!error?.message) {
-    //   console.log(error?.message);
-    //   setIsSuccesful(true);
-    //   console.log(isSuccesful);
-    // }
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (userName || password) {
+            return handleSignUp(userName, password);
+        }
+        // if (!error?.message) {
+        //   console.log(error?.message);
+        //   setIsSuccesful(true);
+        //   console.log(isSuccesful);
+        // }
+    };
 
-  const handleUserNameChange = (e) => {
-    setUserName(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+    const handleUserNameChange = (e) => {
+        setUserName(e.target.value);
+    };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
-  return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1 },
-      }}
-      noValidate
-      autoComplete="off"
-      onSubmit={handleSubmit}
-      // display="flex"
-      // alignItems="center"
-      // justifyContent="center"
-      // bgcolor="warning.main"
-      width="100%"
-      height="100%"
-    >
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Grid item xs={12} md={6} lg={3} m={4} pt={5}>
-          <FormControl variant="filled">
-            <InputLabel htmlFor="component-filled" color="secondary">
-              Username
-            </InputLabel>
-            <FilledInput
-              id="component-filled"
-              type="text"
-              className="input"
-              value={userName}
-              onChange={handleUserNameChange}
-              disabled={loading}
-              color="secondary"
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3} m={3} pt={2}>
-          <FormControl variant="filled">
-            <InputLabel htmlFor="component-filled2" color="secondary">
-              Password
-            </InputLabel>
-            <FilledInput
-              id="component-filled2"
-              type="password"
-              className="input"
-              value={password}
-              onChange={handlePasswordChange}
-              disabled={loading}
-              color="secondary"
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3} m={3} pt={2}>
-          <FormHelperText>{helperText}</FormHelperText>
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            className="submit-btn"
-            disabled={loading}
-          >
-            Register
-          </Button>
-        </Grid>
-        {/* {!error?.message && <p>OKÉ</p>} */}
-      </Grid>
-    </Box>
-  );
+    return (
+        <Box
+            component="form"
+            sx={{
+                "& > :not(style)": { m: 1 },
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+            width="100%"
+            height="100%"
+        >
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Grid item xs={12} md={6} lg={3} m={1} pt={1}>
+                    <Typography variant="h3" gutterBottom component="div">
+                        Registration
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={6} lg={3} m={1} pt={5}>
+                    <FormControl variant="filled">
+                        <InputLabel
+                            htmlFor="component-filled"
+                            color="secondary"
+                        >
+                            Username
+                        </InputLabel>
+                        <FilledInput
+                            id="component-filled"
+                            type="text"
+                            className="input"
+                            value={userName}
+                            onChange={handleUserNameChange}
+                            disabled={loading}
+                            color="secondary"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} md={6} lg={3} m={3} pt={2}>
+                    <FormControl variant="filled">
+                        <InputLabel
+                            htmlFor="component-filled2"
+                            color="secondary"
+                        >
+                            Password
+                        </InputLabel>
+                        <FilledInput
+                            id="component-filled2"
+                            type="password"
+                            className="input"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            disabled={loading}
+                            color="secondary"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} md={6} lg={3} m={3} pt={2}>
+                    <FormHelperText>{helperText}</FormHelperText>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        className="submit-btn"
+                        disabled={loading}
+                    >
+                        Register
+                    </Button>
+                </Grid>
+                {/* {!error?.message && <p>OKÉ</p>} */}
+            </Grid>
+        </Box>
+    );
 };
 
 export default Register;
